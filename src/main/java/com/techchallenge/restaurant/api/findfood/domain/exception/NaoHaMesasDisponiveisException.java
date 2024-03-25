871e632aeebf0e42970c0995ace11cd298264cc9
devopsjeffersantos <devopsjeffersantos@users.noreply.github.com>
2024-03-25 21:14:29 +0000

    deploy: 9851d999ca57b2f61960a15c24c0996420ac6532

--- src/main/java/com/techchallenge/restaurant/api/findfood/domain/exception/NaoHaMesasDisponiveisException.java
+++
@@ -1,7 +0,0 @@
-package com.techchallenge.restaurant.api.findfood.domain.exception;
-
-public class NaoHaMesasDisponiveisException extends RuntimeException {
-    public NaoHaMesasDisponiveisException(String mensagem) {
-        super(mensagem);
-    }
-}
